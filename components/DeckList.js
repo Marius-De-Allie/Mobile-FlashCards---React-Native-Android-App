831a0068b6114050ce5dc470f01ad31039c8e438
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-18 16:13:12 -0400

    Declare DeckList functional component within DeckList.js file

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,2 +1,8 @@
 import React, { Component }from 'react';
 import { View, Text } from 'react-native';
+
+const DeckList = (props) => (
+    <View>
+        <Text>Deck List View Comp</Text>
+    </View>
+); 
