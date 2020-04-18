983a664a1388ab4161cf1a802761bf35c6e58ae8
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-18 16:24:51 -0400

    Declare NewDeck class based component within NewDeck.js file

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+
+class NewDeck extends Component {
+    
+    render() {
+        return (
+            <View>
+                <Text>New Deck View Comp</Text>
+            </View>
+        );
+    }
+};
