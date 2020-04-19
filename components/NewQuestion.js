c031b22d88cd2237644fe34fd39f32f8ca7d68f5
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-18 22:12:07 -0400

    Declare NewQuestion class based component within NewQuestion.js file

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+
+class NewQuestion extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>New Question View Comp</Text>
+            </View>
+        );
+    }
+}
