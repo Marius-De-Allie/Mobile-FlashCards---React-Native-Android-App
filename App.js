ce37298138f03fd529cb7885da51bf7adc09f2ef
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 19:14:53 -0400

    Return NavigationContainer component as root component within App component returned JSX

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ import TabNavigator from './navigation/tabNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
