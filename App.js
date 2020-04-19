0284bc461b208b35ffb1072470bf24b72a4a0f3a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 19:40:00 -0400

    Remove styles variable from App component

--- App.js
+++ App.js
@@ -6,18 +6,8 @@ import TabNavigator from './navigation/tabNavigator';
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
         <TabNavigator />
-      </View>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
