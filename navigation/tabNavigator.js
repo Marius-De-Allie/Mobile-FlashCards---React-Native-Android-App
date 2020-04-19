587f65f13a0a560f9da4656c002859c90a217d17
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 18:40:32 -0400

    Call createMaterialTopTabNavigator function and assign returned value to Tab variable

--- navigation/tabNavigator.js
+++ navigation/tabNavigator.js
@@ -1,3 +1,5 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import DeckList from '../components/DeckList';
 import NewDeck from '../components/NewDeck';
+
+const Tab = createMaterialTopTabNavigator();
