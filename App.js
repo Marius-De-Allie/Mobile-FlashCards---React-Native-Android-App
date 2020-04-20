841ee33d8080e3b6dbbd729f68b252b7943b5939
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 22:07:19 -0400

    Remove TabNavigator component from App component rendered JSX

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import TabNavigator from './navigation/tabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-        <TabNavigator />
     </NavigationContainer>
   );
 }
