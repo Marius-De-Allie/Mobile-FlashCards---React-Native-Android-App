7fe81ac7501bb03d6260b43e3ab6fb282bc2dd0d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 22:11:11 -0400

    Return MainStackNavigator component as child component to NavigatorContainer component within App component rendered JSX

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import MainStackNavigator from './navigation/stackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <MainStackNavigator />
     </NavigationContainer>
   );
 }
