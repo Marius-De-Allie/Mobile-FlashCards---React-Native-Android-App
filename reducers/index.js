c6976746cdeb26bb0a89576c37a3005c19c0488f
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-20 16:52:18 -0400

    Add RECEIVE_DECKS case to reducer at 'reducers/index.js' file

--- reducers/index.js
+++ reducers/index.js
@@ -2,6 +2,11 @@ import { RECEIVE_DECKS } from '../actions';
 
 const decks = (state = {}, action) => {
     switch(action.type) {
+        case RECEIVE_DECKS:
+            return {
+                ...state,
+                ...action.decks
+            }
 
         default:
             return state;
