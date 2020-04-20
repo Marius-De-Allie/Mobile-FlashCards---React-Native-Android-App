c67a2f5dcb0c02ba3f00c44cc9c5e7de39c1d661
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 23:34:28 -0400

    Import applyMiddleware from redux into App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import MainStackNavigator from './navigation/stackNavigator';
 import reducer from './reducers';
 import logger from './middleware/logger';
+
 
 export default function App() {
   return (
