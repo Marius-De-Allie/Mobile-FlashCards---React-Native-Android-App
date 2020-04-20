2b44626776fb01ddaaf4029991ceec73b0df4155
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 23:33:57 -0400

    Import logger middleware into App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStore } from 'redux';
 import MainStackNavigator from './navigation/stackNavigator';
 import reducer from './reducers';
+import logger from './middleware/logger';
 
 export default function App() {
   return (
