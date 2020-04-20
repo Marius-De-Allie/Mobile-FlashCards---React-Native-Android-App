889915968d51cd8b5797ab532a67bc8316ba21c5
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 23:17:49 -0400

    Import createStore function from redux into App.js file

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStore } from 'redux';
 import MainStackNavigator from './navigation/stackNavigator';
 
 export default function App() {
