c2688b6c5dd93737992c2fca25bf5f2cf7d76caf
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-19 20:15:44 -0400

    Import NewQuestion component into stackNavigator.js file

--- navigation/stackNavigator.js
+++ navigation/stackNavigator.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import Deck from '../components/Deck';
 import Quiz from '../components/Quiz';
+import NewQuestion from '../components/NewQuestion';
 
 const Stack = createStackNavigator();
 
