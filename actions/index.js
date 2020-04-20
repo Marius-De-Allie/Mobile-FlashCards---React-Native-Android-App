f1d05ffd4d5444212bd11f4696c223afa54ba8ad
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-20 14:37:24 -0400

    Declare handeReceiveDecks thunk action creatorwhich calls _getDecks asycn function and passes result to receiveDecks action creator

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,21 @@
 import { _getDecks } from '../_DATA';
+
 const RECEIVE_DECKS = 'RECEIVE_DECKS';
 
 const receiveDecks = (decks) => ({
     type: RECEIVE_DECKS,
     decks
 });
+
+// Thunk action creator.
+const handleReceiveDecks = async () => {
+    return(dispatch) => {
+        try {
+            const decks = await _getDecks();
+            receiveDecks(decks);
+        } catch(e) {
+            console.log('Error retreiving decks, please try again', e);
+        }
+    }
+};
 
