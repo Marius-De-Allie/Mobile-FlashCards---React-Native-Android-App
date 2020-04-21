8f1ead2322c9bb8204f9fd6044c6dbae029c3c1e
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-21 01:10:34 -0400

    Call useNavigation passing result to navigation variable

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -3,15 +3,19 @@ import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
-const DeckListItem = ({title, cards}) => (
-    <View>
-        <TouchableWithoutFeedback>
-            <View>
-                <Text><MaterialCommunityIcons name="cards" />{title}</Text>
-                <Text>{`${cards} card(s)`}</Text>
-            </View>
-        </TouchableWithoutFeedback>
-    </View>
-);
+const DeckListItem = ({title, cards}) => {
+    const navigation = useNavigation();
+    return (
+        <View>
+            <TouchableWithoutFeedback>
+                <View>
+                    <Text><MaterialCommunityIcons name="cards" />{title}</Text>
+                    <Text>{`${cards} card(s)`}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        </View>
+    );
+};
+
 
 export default DeckListItem;
