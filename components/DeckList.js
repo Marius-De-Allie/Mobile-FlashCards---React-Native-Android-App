66865b15282989c43d03cdbb8fd5881c3672f4ce
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-20 22:55:25 -0400

    Import DeckListItem custom component into '/components/DeckList.js' file

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,12 +1,13 @@
 import React, { Component }from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import DeckListItem from './DeckListItem';
 
 const DeckList = (props) => {
     const { decks } = props;
     // Array of all decks.
     const deckIds = Object.keys(decks);
-    console.log('DECKLIST', props);
+    console.log('DECKIDS', deckIds);
     return (
         <View>
             <Text>Deck List View Comp</Text>
