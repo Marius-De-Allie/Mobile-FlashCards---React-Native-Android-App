a8418e9de63c271622697e7981307aeaca8b749a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-20 23:52:40 -0400

    Destructure title and cards properties from props object as argument for DeckListItem functional component

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const DeckListItem = () => (
+const DeckListItem = ({title, cards}) => (
     <View>
         <Text>DeckListItem Comp</Text>
     </View>
