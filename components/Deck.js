46b2ce63a9718fbfb5917c621e76a497b721dfbb
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-21 16:43:31 -0400

    Destructure route and deckId properties from props object within Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
-const Deck = () => (
-    <View>
-        <Text>Deck Comp</Text>
-    </View>
-);
-
+const Deck = ({routes, deckId}) => {
+    return (
+        <View>
+            <Text>Deck Comp</Text>
+        </View>
+    );
+};
 // Access the following store state as props on this component.
 const mapStateToProps = (state) => ({
     decks: state
