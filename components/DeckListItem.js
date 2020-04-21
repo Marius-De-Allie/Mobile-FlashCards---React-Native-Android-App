d1f38dc9aa29ec9004c3c3fe403df9923a25efea
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-21 01:07:24 -0400

    Import TouchableWithoutFeedback component into DeckListItem component and render TouchableWithoutFeedback component

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const DeckListItem = ({title, cards}) => (
     <View>
-        <Text><MaterialCommunityIcons name="cards" />{title}</Text>
-        <Text>{`${cards} card(s)`}</Text>
+        <TouchableWithoutFeedback>
+            <View>
+                <Text><MaterialCommunityIcons name="cards" />{title}</Text>
+                <Text>{`${cards} card(s)`}</Text>
+            </View>
+        </TouchableWithoutFeedback>
     </View>
 );
 
