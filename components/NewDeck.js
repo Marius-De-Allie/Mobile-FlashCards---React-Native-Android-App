22b4287be1f9e8d934d0b494737774e4ad9c795e
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-21 21:14:37 -0400

    Import KeyboardAvaoidingView and TextInput components into '/components/NewDeck.js' file

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, KeyboardAvoidingView, TextInput } from 'react-native';
 import { connect } from 'react-redux';
 
 class NewDeck extends Component {
@@ -7,7 +7,7 @@ class NewDeck extends Component {
     render() {
         return (
             <View>
-                <Text>New Deck View Comp</Text>
+                <Text>Create New Deck</Text>
             </View>
         );
     }
