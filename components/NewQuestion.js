15f00153aca8399438495e65d8d4e5d423c96455
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-22 21:19:38 -0400

    Destructure deckId property within onCardSubmit function in NewQuestion component

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -30,6 +30,7 @@ class NewQuestion extends Component {
 
     onCardSubmit = () => {
         const {question, answer} = this.state;
+        const {deckId} = this.props;
         // Check whether both input fields are not empty.
         if(question !== '' && answer !== '') {
             // if they are not empty, create a question object.
