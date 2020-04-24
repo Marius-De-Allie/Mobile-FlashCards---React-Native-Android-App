25eb995cc141672fb03047b5c8d7e24f0550bdb5
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-24 02:49:20 -0400

    Import TOGGLE_ANSWERED into '/reducers/index.js' file

--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions';
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD, TOGGLE_ANSWERED } from '../actions';
 
 const decks = (state = {}, action) => {
     switch(action.type) {
