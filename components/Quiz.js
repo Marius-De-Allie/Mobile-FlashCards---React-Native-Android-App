03ec07659510a275b68094aa3c13638d28bb4259
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-23 20:10:18 -0400

    Declare state object within Quiz component, state contains the properties, showAnswer, page, answer and correct

--- components/Quiz.js
+++ components/Quiz.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 class Quiz extends React.Component {
+    // Component state.
+    state = {
+        showAnswer: false,
+        page: 0,
+        answer: '',
+        correct: 0
+    }
 
     render() {
         return (
