1da4110b4c96db2bf350bbfe15545cda2184cbe1
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-23 20:07:43 -0400

    Convert Quiz component from a functional component to a class based component

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Quiz = () => (
-    <View>
-        <Text>Quiz Comp</Text>
-    </View>
-);
+class Quiz extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Quiz Comp</Text>
+            </View>
+        );    
+    }
+};
 
 export default Quiz;
