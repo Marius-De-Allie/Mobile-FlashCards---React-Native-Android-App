9d77d74ea81730aefc47cdaa5d33492a7e169b29
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-25 16:19:57 -0400

    Import ADD_USERANSWER into '/reducers/index.js' file

--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DECKS, ADD_DECK, ADD_CARD, TOGGLE_ANSWERED } from '../actions';
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD, TOGGLE_ANSWERED, ADD_USERANSWER } from '../actions';
 
 const decks = (state = {}, action) => {
     switch(action.type) {
