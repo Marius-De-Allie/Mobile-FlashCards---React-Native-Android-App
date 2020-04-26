be26fe1090cd330bc675d3a4a1a3687e3e5f810b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-26 19:34:28 -0400

    Update backgroundColor property value from 'lightblue' to '#3498db' on deckTextContainer property within styles object in Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         paddingHorizontal:  50,
         paddingVertical: 15,
-        backgroundColor: 'lightblue',
+        backgroundColor: '#3498db',
         borderRadius: 5,
         marginVertical: 30,
     },
