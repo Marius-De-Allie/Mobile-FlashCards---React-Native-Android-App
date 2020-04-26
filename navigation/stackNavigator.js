f28d674631db5d527f7ea682a3239ab55a21ab6f
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-25 21:31:58 -0400

    Update component property value of MainStackNavigator's home screen stack component from TabNavigator to DeckList

--- navigation/stackNavigator.js
+++ navigation/stackNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Deck from '../components/Deck';
 import Quiz from '../components/Quiz';
 import NewQuestion from '../components/NewQuestion';
+import DeckList from '../components/DeckList';
 import TabNavigator from './tabNavigator';
 
 const Stack = createStackNavigator();
@@ -11,7 +12,7 @@ const MainStackNavigator = () => (
     <Stack.Navigator
         initialRouteName="Home"
     >
-        <Stack.Screen name="Home" component={TabNavigator} />
+        <Stack.Screen name="Home" component={DeckList} />
         <Stack.Screen name="Deck" component={Deck} />
         <Stack.Screen name="Quiz" component={Quiz} />
         <Stack.Screen name="New Card" component={NewQuestion} />
