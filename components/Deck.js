f4f8077c8c0e6ad1c984f2c62e4174d7079dd92d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-26 16:44:58 -0400

    Import StyleSheet component into '/components/Deck.js' file

--- components/Deck.js
+++ components/Deck.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
 const Deck = ({route, decks, navigation}) => {
