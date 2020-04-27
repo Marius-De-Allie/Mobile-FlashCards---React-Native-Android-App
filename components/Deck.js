88181cacbbd35bc606d95e6a3b97452a3f2c09dc
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-26 22:29:14 -0400

    Add marginTop property to btnsContainer property on styles object within Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
         paddingVertical: 15,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        marginTop: 30
     },
     buttons: {
         backgroundColor: '#2ecc71',
