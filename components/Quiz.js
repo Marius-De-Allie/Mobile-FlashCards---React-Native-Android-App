961237b6ca49e10d69d59c0a7a5775cf3a65733e
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-27 01:25:32 -0400

    Import MaterialCommunityIcons into '/components/Quiz.js' file

--- components/Quiz.js
+++ components/Quiz.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import RadioGroup, { Radio } from 'react-native-radio-input';
 import { connect } from 'react-redux';
-import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+import { MaterialIcons, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 import { selectAnswer, resetDeck } from '../actions';
 
 class Quiz extends React.Component {
