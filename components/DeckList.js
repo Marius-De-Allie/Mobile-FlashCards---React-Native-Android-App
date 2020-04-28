d86104e0c9a051950018ab13520760bab54b2076
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-28 01:22:23 -0400

    Replace FlatList component import with ScrollView component import in DeckList component

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,5 +1,5 @@
 import React, { Component }from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import DeckListItem from './DeckListItem';
 
