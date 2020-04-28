b4be9bab1e797a36bfe42e3e0d44d694460db3b2
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-27 23:35:11 -0400

    Create styles object within NewQuestion component, object contains container property

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -79,4 +79,15 @@ const mapStateToProps = (state, ownProps) => ({
     deckId: ownProps.route.params.deckId
 });
 
+// style object.
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginHorizontal: 30,
+        marginVertical: 30
+    }
+});
+
 export default connect(mapStateToProps)(NewQuestion);
