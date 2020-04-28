8d3258d778563488377dff1730b90585b84b9304
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-27 22:26:51 -0400

    Import StyleSheet component into '/components/NewDeck.js' file

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, KeyboardAvoidingView, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { addDeck } from '../actions';
 
