e2d05baeb44f7885bfb4fa7be1e19c837625ba15
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-29 01:44:04 -0400

    Declare setAsyncData function within api.js file

--- utils/api.js
+++ utils/api.js
@@ -1,2 +1,13 @@
-import AsyncStorage from '@react-native-community/async-storage';
-import { store } from '../App.js';
+import { AsyncStorage } from 'react-native';
+import { store } from '../App';
+
+// Setup asyncStorage item to hold app data.
+export const setAsyncData = async () => {
+    try {
+        await AsyncStorage.setItem('decks', JSON.stringify(store.getState()));
+    } catch(e) {
+        console.log('Unable to create AsyncStorage item');
+    }
+};
+
+
