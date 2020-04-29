2355288d897896ddc4499512b4f6938b9f7ad695
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-29 02:21:40 -0400

    Call getAsyncData function within App component's componentDidMount lifecycle method and dispatch handleReceiveDecks thunk action creator if no data was returned by getAsyncData

--- App.js
+++ App.js
@@ -9,17 +9,31 @@ import reducer from './reducers';
 import logger from './middleware/logger';
 import { handleReceiveDecks } from './actions';
 import { setAsyncData, getAsyncData } from './utils/api';
+import { AsyncStorage } from 'react-native';
+
 
 // Create redux store.
 export const store = createStore(reducer, applyMiddleware(thunk, logger));
 // Call setAsyncData function to create AsyncStorage decks item.
 setAsyncData();
 
+
+
+
 class App extends React.Component {
 
   componentDidMount() {
-    // Call thunk action creator to get initial decks data and update store.
-    store.dispatch(handleReceiveDecks());
+    // Return 'decks' asyncStorage item.
+    getAsyncData()
+    .then(data => {
+      if(data === null || Object.keys(data).length <= 0) {
+        // Call thunk action creator to get initial decks data and update store.
+        store.dispatch(handleReceiveDecks());
+
+      } else {
+          // Do nothing
+      }
+    })
   }
 
   render() {
