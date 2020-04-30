251ea04b0b57675007a893b9c57e50af09dfef0c
Marius-De-Allie <spectre4300s@yahoo.com>
2020-04-30 04:12:00 -0400

    Add initialLayout prop to Navigator component in TabNavigator component prop value set to {width: Dimensions.get('window').width}

--- navigation/tabNavigator.js
+++ navigation/tabNavigator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import MainStackNavigator, { NewDeckStack } from './stackNavigator';
 
@@ -7,6 +8,7 @@ const Tab = createMaterialTopTabNavigator();
 const TabNavigator = () => (
     <Tab.Navigator
         initialRouteName="Decks"
+        initialLayout={{width: Dimensions.get('window').width}}
     >
         <Tab.Screen name="Decks" component={MainStackNavigator} />
         <Tab.Screen name="New Deck" component={NewDeckStack} />
